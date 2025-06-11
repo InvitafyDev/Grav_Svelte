67b2a543f2aeb619589a6ac93c3868d5e063af17
Kike <fourenrique@gmail.com>
2025-06-11 15:35:15 -0600

    try2

--- src/routes/inputs/+page.svelte
+++ src/routes/inputs/+page.svelte
@@ -147,7 +147,7 @@
                 label="Text Input with Icon and Password"
                 icon="fas fa-user"
                 bind:valueVar={textInputWithIcon}
-                validation={true}
+                validation={false}
             />
             <span class="block mt-1 text-sm text-gray-600"
                 >Value: {textInputWithIcon}</span
