36750480c7725acfa403d329451a7c34cb16330a
Kike <fourenrique@gmail.com>
2025-09-26 10:22:20 -0600

    Update ImageModal.svelte

--- src/lib/CRUD/ImageModal.svelte
+++ src/lib/CRUD/ImageModal.svelte
@@ -1,16 +1,34 @@
 <script lang="ts">
-    import { Grav_Modal, closeModal } from '$lib/Modals/index.js';
+    import { Grav_Modal, closeModal } from "$lib/Modals/index.js";
     export let src: string;
-    const MODAL_ID = 'crud-image-modal';
+    const MODAL_ID = "crud-image-modal";
 </script>
 
 <Grav_Modal
     title="Imagen"
-    size="md"
+    size="lg"
     isVista={true}
     onClose={() => closeModal(MODAL_ID)}
 >
-    <div class="flex items-center justify-center p-4">
-        <img src={src} alt="image" class="max-w-full max-h-[80vh]" />
+    <div class="image-modal-container">
+        <img {src} alt="image" class="image-modal-img" />
     </div>
 </Grav_Modal>
+
+<style>
+    .image-modal-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+    }
+
+    .image-modal-img {
+        max-height: 85vh;
+        max-width: 100%;
+        height: auto;
+        width: auto;
+        object-fit: contain;
+    }
+</style>
